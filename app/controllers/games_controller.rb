cdbdce17fe62e9966a9e6b9a6a3ea2c6954a6ac2
Camille Regnault <c.regnault.pro@gmail.com>
2019-05-16 16:34:17 +0200

    create longest word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10) * ' '
+  end
+
+  def letter_in_grid(your_word)
+    your_word.chars.sort.all? { |letter| @letters.include?(letter) }
+  end
+
+  def score
+    raise
+    @letters = params[:letters]
+    @your_word = params[:your_word].upcase
+
+    url = "https://wagon-dictionary.herokuapp.com/#{@your_word}"
+    word_serialized = open(url).read
+    word = JSON.parse(word_serialized)
+
+    case
+    when @your_word.length == 0 || @your_word.length > 10
+      @result = "Sorry but #{@your_word} can't be built out of the letters"
+    when !word['found']
+      @result = "Sorry but #{@your_word} does not seem to be valid English word..."
+    when letter_in_grid(@your_word)
+      @result = "wrong"
+    else @result = "Congratulations!!!!"
+    end
+  end
+end
